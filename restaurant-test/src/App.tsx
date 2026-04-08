695cd2c909de8ce3e2a5da5b0df2d72a12300cf0
nestorka <nestorgg6@gmail.com>
2026-04-08 17:20:44 +0200

    feat: Search function

--- restaurant-test/src/App.tsx
+++ restaurant-test/src/App.tsx
@@ -1,17 +1,23 @@
 import SearchBar from "./components/SearchBar.tsx";
 import {useRestaurants} from "./hooks/useRestaurants.ts";
 import RestaurantList from "./components/RestaurantListProps.tsx";
+import {useState} from "react";
 
 export default function App() {
-    const postcode = 'EC4M7RF'
-    const { data } = useRestaurants(postcode)
+    const [searchInput, setSearchInput] = useState('');
+    const [postcode, setPostcode] = useState('');
+
+    const {data} = useRestaurants(postcode)
+    const findRestaurants = () => {
+        setPostcode(searchInput.replace(/\s/g, ''));
+    }
     return (
         <div className="min-h-screen bg-gray-50">
             <div className="max-w-2xl mx-auto px-4 py-10">
                 <h1 className="text-3xl font-bold text-gray-800 mb-6">Find Restaurants</h1>
-                <SearchBar value={""} onChange={console.log} onSearch={console.log}/>
+                <SearchBar value={searchInput} onChange={setSearchInput} onSearch={findRestaurants}/>
                 <div className="mt-8 flex flex-col gap-4">
-                    <RestaurantList restaurants={data ?? []} />
+                    <RestaurantList restaurants={data ?? []}/>
                 </div>
             </div>
         </div>
